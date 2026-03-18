143b0d034e1b2426c2aac0c60a0d183917f80905
nyatoru <nyatoru@example.com>
2026-03-18 15:35:05 +0800

    Add AbstractFeature base class and update WoodcuttingFeature
    - Create AbstractFeature base class extending Feature interface
    - Provides common implementation for getId(), getName(), isEnabled()
    - Reduces boilerplate code in feature classes
    - Update WoodcuttingFeature to extend AbstractFeature

--- src/main/java/com/nyarutoru/nekoplugin/features/woodcutting/WoodcuttingFeature.java
+++ src/main/java/com/nyarutoru/nekoplugin/features/woodcutting/WoodcuttingFeature.java
@@ -1,53 +1,37 @@
 package com.nyarutoru.nekoplugin.features.woodcutting;
 
 import com.nyarutoru.nekoplugin.NekoPlugin;
-import com.nyarutoru.nekoplugin.core.Feature;
-import org.bukkit.event.HandlerList;
+import com.nyarutoru.nekoplugin.core.AbstractFeature;
 
 /**
  * Woodcutting feature - enhanced wood processing using stonecutter.
  * Allows converting logs to all wood-related items in the stonecutter:
  * planks, stairs, slabs, fences, doors, trapdoors, pressure plates, buttons, signs.
  */
-public class WoodcuttingFeature implements Feature {
+public class WoodcuttingFeature extends AbstractFeature {
 
-    public static final String ID = "woodcutting";
-    public static final String NAME = "Woodcutting";
-
-    private boolean enabled = false;
     private WoodOnStoneCutter woodOnStoneCutter;
 
-    @Override
-    public String getId() {
-        return ID;
+    public WoodcuttingFeature() {
+        super("woodcutting", "Woodcutting");
     }
 
     @Override
-    public String getName() {
-        return NAME;
-    }
-
-    @Override
-    public void onEnable(NekoPlugin plugin) {
+    protected void onEnable() {
+        NekoPlugin plugin = NekoPlugin.getInstance();
+        
         // Register stonecutter recipes
         woodOnStoneCutter = new WoodOnStoneCutter(plugin);
         woodOnStoneCutter.registerRecipes();
 
-        this.enabled = true;
-        
         plugin.getLogger().info("Woodcutting feature enabled");
     }
 
     @Override
     public void onDisable() {
+        super.onDisable();
         if (woodOnStoneCutter != null) {
             woodOnStoneCutter.removeRecipes();
         }
-        this.enabled = false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
     }
 }
